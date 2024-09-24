d3da8186b4489a70cbaa951c687a3a2f950832e6
Sander Jyhne <sandsjyhne@gmail.com>
2024-09-24 07:55:39 +0000

    added resnet version of the model with more weights

--- main.py
+++ main.py
@@ -1,15 +1,67 @@
 import pathlib
 import argparse
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
 
-import lightning as L
-from lightning.pytorch.callbacks import ModelCheckpoint
-from models.lightning_crop import Crop
-from models.crop_model import CropModel
-from models.lightning_yield import Yield
+from models.crop_model import CropModel, ResNetCropModel
 from models.yield_model import YieldModel
 
 
 from loader.crop_dataset import get_crop_dataset_loader, get_yield_dataset_loader
+
+
+def train_one_epoch(model, optimizer, train_loader, cuda_idx=0):
+    model.train()
+    
+    training_acc = list()
+    training_loss = list()
+    
+    for idx, (images, labels, info) in tqdm(enumerate(train_loader), total=len(train_loader)):
+        
+        images = images.to(f"cuda:{cuda_idx}")
+        labels = labels.to(f"cuda:{cuda_idx}")
+        
+        optimizer.zero_grad()
+
+        preds = model(images)
+        
+        predictions = torch.argmax(torch.softmax(preds, dim=1), dim=1)
+        
+        loss = F.cross_entropy(preds, labels)
+        
+        training_loss.append(loss.item())
+        training_acc.append(torch.sum(predictions == labels).item() / len(labels))
+        
+        loss.backward()
+        optimizer.step()
+    
+    print(f"Train accuracy: {sum(training_acc)/len(training_acc)}")
+    print(f"Train loss: {sum(training_loss)/len(training_loss)}")
+    
+def validate(model, val_loader, cuda_idx=0):
+    model.eval()
+    
+    validation_acc = list()
+    validation_loss = list()
+    
+    for _, (images, labels, info) in tqdm(enumerate(val_loader), total=len(val_loader)):
+        
+        images = images.to(f"cuda:{cuda_idx}")
+        labels = labels.to(f"cuda:{cuda_idx}")
+        
+        preds = model(images)
+        
+        predictions = torch.argmax(torch.softmax(preds, dim=1), dim=1)
+        
+        loss = F.cross_entropy(preds, labels)
+        
+        validation_loss.append(loss.item())
+        validation_acc.append(torch.sum(predictions == labels).item() / len(labels))
+        
+        
+    print(f"Validation accuracy: {sum(validation_acc)/len(validation_acc)}")
+    print(f"Validation loss: {sum(validation_loss)/len(validation_loss)}")
 
 if __name__ == "__main__":
 
@@ -17,16 +69,32 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, default="crop")
     args = parser.parse_args()
 
+    learning_rate = 0.0001
+
     if args.model == "crop":
         train_loader = get_crop_dataset_loader("data/crop/train", 8)
         val_loader = get_crop_dataset_loader("data/crop/test", 1, shuffle=False)
         model = CropModel((17, 12, 25, 25), 4)
-        lightning_model = Crop(model, 0.0001)
+        model = ResNetCropModel((17, 12, 25, 25), 4)
     elif args.model == "yield":
         train_loader = get_yield_dataset_loader("data/yield/train", 8)
         val_loader = get_yield_dataset_loader("data/yield/test", 1, shuffle=False)
         model = YieldModel(17, 12, None)
-        lightning_model = Yield(model, 0.0001)
+
+    cuda_idx = 3
+
+    model = model.to(f"cuda:{cuda_idx}")
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    epochs = 100
+    
+    for epoch in range(epochs):
+        train_one_epoch(model, optimizer, train_loader, cuda_idx=cuda_idx)
+        validate(model, val_loader, cuda_idx=cuda_idx)
+    
+
+    """
 
     checkpoint_callback = ModelCheckpoint(
         monitor='val_loss',
@@ -38,3 +106,4 @@ if __name__ == "__main__":
 
 
     trainer.fit(lightning_model, train_loader, val_loader)
+    """
